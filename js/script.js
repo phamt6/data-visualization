47beada4585c6a54f6c5f4e1cf4c96ceb35302c5
Tien Hoang Pham <tienhoang.pham@gmail.com>
2020-12-01 20:21:53 +0200

    Update scatter plot

--- js/script.js
+++ js/script.js
@@ -3,23 +3,10 @@ site = {};
 site.utils = (() => {
     let data = null;
 
-    const extractByCountry = (data, country, year = 2017) => {
-        const filtered = country ?
-            data.filter(each => each.LOCATION === country && each.TIME == year) :
-            data.filter(each => each.TIME == year)
-
-        return filtered.map(each => {
-            return {
-                country: each.LOCATION,
-                value: Number(each.Value) * 100000
-            }
-        });
-    }
-
     const getData = () => {
         if (data) { return Promise.resolve(data); }
         else {
-            return d3.csv('https://phamt6.github.io/data/finland_income.csv')
+            return d3.csv('https://phamt6.github.io/data/gapminder.csv')
                 .then(d => {
                     data = d;
                     return data;
@@ -28,7 +15,6 @@ site.utils = (() => {
     }
 
     return {
-        extractByCountry,
         getData
     }
 })();
@@ -38,16 +24,15 @@ site.bar = (() => {
     let country = null;
 
     const createBarChart = data => {
-        const extractedData = site.utils.extractByCountry(data, country, year);
-
         const height = 500;
-        const width = 1000;
+        const width = 2000;
         const margin = { top: 50, left: 50, right: 50, bottom: 50 };
 
         const xAxis = g => g
             .attr('transform', `translate(0, ${height - margin.bottom})`)
+            .attr('class', 'rotate')
             .attr('font-size', '20px')
-            .call(d3.axisBottom(x).tickFormat(i => extractedData[i].country))
+            .call(d3.axisBottom(x).tickFormat(i => data[i].country))
 
         const svg = d3.select('#barChart')
             .append('svg')
@@ -55,23 +40,23 @@ site.bar = (() => {
             .attr('width', width);
 
         const x = d3.scaleBand()
-            .domain(d3.range(extractedData.length))
+            .domain(d3.range(data.length))
             .range([margin.left, width - margin.right])
             .padding(0.1);
 
         const y = d3.scaleLinear()
-            .domain([0, 100000])
+            .domain([0, 100])
             .range([height - margin.bottom, margin.top])
 
         svg
             .append('g')
             .attr('fill', 'royalblue')
             .selectAll('rect')
-            .data(extractedData.sort((a, b) => d3.descending(a.value, b.value)))
+            .data(data.sort((a, b) => d3.descending(a.life_exp, b.life_exp)))
             .join('rect')
             .attr('x', (d, i) => x(i))
-            .attr('y', d => y(d.value))
-            .attr('height', d => y(0) - y(d.value))
+            .attr('y', d => y(d.life_exp))
+            .attr('height', d => y(0) - y(d.life_exp))
             .attr('width', x.bandwidth())
 
         svg.node();
@@ -94,10 +79,8 @@ site.bar = (() => {
 
 site.scatter = (() => {
     const createScatter = data => {
-        const extractedData = site.utils.extractByCountry(data);
-
-        const height = 500;
-        const width = 1000;
+        const height = 2000;
+        const width = 2000;
         const margin = { top: 50, left: 50, right: 50, bottom: 50 };
 
         const svg = d3.select('#scatter')
@@ -107,11 +90,25 @@ site.scatter = (() => {
 
         svg
             .selectAll('circle')
-            .data(extractedData)
+            .data(data)
+            .enter()
             .append('circle')
-            .attr('r', 15)
-            .attr('cx', d => d.value)
-            .attr('class', 'scatter_circle');
+            .attr('r', d => d.population / (10 ** 6) / 10)
+            .attr('cy', d => d.life_exp * 10)
+            .attr('cx', d => d.gdp_cap / 30)
+            .attr('fill', 'royalblue')
+            .attr('opacity', '0.5');
+
+        svg
+            .selectAll('text')
+            .data(data)
+            .enter()
+            .append('text')
+            .text(d => d.country)
+            .attr('y', d => d.life_exp * 10)
+            .attr('x', d => d.gdp_cap / 30)
+            .attr('font-size', '11px')
+            .attr('fill', 'red');
 
         svg.node();
     }
@@ -128,9 +125,5 @@ site.scatter = (() => {
 
 site.bar.init();
 site.scatter.init();
-
-document.getElementById('slider_year').addEventListener('change', e => {
-    site.bar.init(e.target.value);
-})
 
 
